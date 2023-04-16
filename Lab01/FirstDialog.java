1d055f71e666ebf91bcc974f67b80cda0d52008e
hello215178 <nmaianh10102003@gmail.com>
2023-04-16 12:05:14 +0700

    first commit

---
+++ Lab01/FirstDialog.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lesson_1_javalab;
+
+/**
+ *
+ * @author DELL
+ */
+import javax.swing.JOptionPane;
+public class FirstDialog {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Hello world! How are you?");
+        System.exit(0);
+    }
+}
---
+++ Lab01/Twovariables_linearsystem.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lesson_1_javalab;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DELL
+ */
+public class Twovariables_linearsystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a11, a12, a21, a22, b1, b2;
+        System.out.println("Enter a11, a12, b1 in the equations a11x1 + a12x2 = b1 ");
+        
+            System.out.print("a11 = ");
+            a11 = sc.nextDouble();
+            System.out.print("a12 = ");
+            a12 = sc.nextDouble();
+            System.out.print("b1 = ");
+            b1 = sc.nextDouble();
+            
+        System.out.println("Enter a21, a22, b2 in the equations a22x1 + a21x2 = b2 ");
+        
+            System.out.print("a21 = ");
+            a21 = sc.nextDouble();
+            System.out.print("a22 = ");
+            a22 = sc.nextDouble();
+            System.out.print("b2 = ");
+            b2 = sc.nextDouble();
+        
+        double D = a11 * a22 - a21 * a12;
+        double D1 = a22 * b1 - b2 * a12;
+        double D2 = a11 * b2 - a21 * b1;
+        if (D == 0) 
+        {
+            if (D1+D2!=0)
+            System.out.println("The equation system has no root!");
+            else System.out.println("The equation system has infinitely many roots");
+        }
+        else System.out.println("The root of the linear system: x1 = " + D1 / D + ", x2 = " + D2 / D);
+       
+        sc.close();
+    }
+}
